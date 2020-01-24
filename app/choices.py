20eedf477d3b7e1328979e5d719dd4d1850628ba
Victorvick399 <victorvick399@gmail.com>
2020-01-24 12:41:27 +0300

    create choice for my model fields

---
+++ app/choices.py
@@ -0,0 +1,27 @@
+GENDER_CHOICES = (
+    (1, "Male"),
+    (2, "Female"),
+    (3, "Transgender"),
+)
+COMPLEXION_CHOICES = (
+    (1, "Brown"),
+    (2, "Chocolate"),
+    (3, "Dark")
+)
+PERSONALITY_CHOICES = (
+    (1, "Introvert"),
+    (2, "Extrovert"),
+)
+HEIGHT_CHOICES = (
+    (1, "4 feet"),
+    (2, "5 feet"),
+    (3, "6 feet"),
+    (4, "7 feet")
+)
+AGE_CHOICES = (
+    (1, "18-25"),
+    (2, "26-30"),
+    (3, "30-35"),
+    (4, "36-40"),
+    (5, "Above 40")
+)
